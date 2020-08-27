85207b037cf2660d9be8711b47e6d9b1497b6c7a
luisfvsanchez <luisfvsanchez@hotmail.com>
2020-08-27 14:25:27 -0300

    github pages files

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ class App extends Component {
       monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox placeholder="Search Monsters"
           handleChange={this.handleChange}>
         </SearchBox>
