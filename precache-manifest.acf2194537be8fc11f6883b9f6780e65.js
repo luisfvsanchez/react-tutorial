313f0c622788c817f9a98e37c92fa69eaaf638cd
luisfvsanchez <luisfvsanchez@hotmail.com>
2020-08-27 14:24:56 -0300

    Updates

---
+++ precache-manifest.acf2194537be8fc11f6883b9f6780e65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a906d54d10badb4324eec218fca4e51e",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "66f953d9cab98eb0b4b2",
+    "url": "/react-tutorial/static/css/main.60a10b76.chunk.css"
+  },
+  {
+    "revision": "a1e21c801902c63e1988",
+    "url": "/react-tutorial/static/js/2.0ebfce24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.0ebfce24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66f953d9cab98eb0b4b2",
+    "url": "/react-tutorial/static/js/main.5cfe09ae.chunk.js"
+  },
+  {
+    "revision": "ea70b2289830dd83bed2",
+    "url": "/react-tutorial/static/js/runtime-main.f67bfce7.js"
+  }
+]);
